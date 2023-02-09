da79ed98ae8014b9cf765044a2b2f162694be290
VlC <victorstancioiu@gmail.com>
2023-02-09 21:50:03 +0100

    begin adding game icons

--- menus/single_game_chooser.gd
+++ menus/single_game_chooser.gd
@@ -3,6 +3,10 @@ extends Control
 @onready var sfx: AudioStreamPlayer = $AudioSFX
 
 
+func _ready():
+	print($GridContainer/DrawTrampoline.get_meta("a").waluigi)
+
+
 func _on_button_back_button_down():
 	sfx.set_stream(DBs.sounds.negative)
 	sfx.play()
