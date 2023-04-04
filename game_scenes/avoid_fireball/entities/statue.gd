1ef8a17b512ff7fb0a8063cecbc200b8f4816fce
VlC <victorstancioiu@gmail.com>
2023-04-04 12:48:06 +0200

    add moving animation to avoid fireball mgm statue

---
+++ game_scenes/avoid_fireball/entities/statue.gd
@@ -0,0 +1,29 @@
+extends PathFollow2D
+
+@onready var sfx: AudioStreamPlayer2D = $AudioStreamPlayer2D
+var moving_grinding_sfx = preload("res://game_scenes/avoid_fireball/assets/audio/NCS_SE_VS_YOKE_KUPPA_LV.ogg")
+
+var tween: Tween
+
+
+func _ready() -> void:
+	randomize()
+	move_randomly()
+
+func _process(delta: float) -> void:
+	pass
+
+
+func move_randomly() -> void:
+	if tween:
+		tween.kill()
+	tween = create_tween()
+	tween.finished.connect(move_finished)
+	sfx.stream = moving_grinding_sfx
+	sfx.play()
+	tween.tween_property(self, "progress_ratio", randf(), 4).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+
+
+func move_finished() -> void:
+	sfx.stop()
+	$AnimationPlayer.play("start_fire")
