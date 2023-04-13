3a66f9b469693d26ce03583cf4bae9807bdc63af
VlC <victorstancioiu@gmail.com>
2023-04-13 11:04:36 +0200

    whack mole mgm redo part 1

--- game_scenes/whack_mole/whack_mole.gd
+++ game_scenes/whack_mole/whack_mole.gd
@@ -1,29 +1,12 @@
 extends Node
 
-const SOUNDS: Dictionary = {
-	"mole_hit": preload("res://game_scenes/whack_mole/assets/audio/NCS_SE_VS_MOG_CHOROHIT.ogg"),
-	"luigi_hit_0": preload("res://game_scenes/whack_mole/assets/audio/NCS_SE_VS_MOG_LUIGIHIT_0.ogg"),
-	"luigi_hit_1": preload("res://game_scenes/whack_mole/assets/audio/NCS_SE_VS_MOG_LUIGIHIT_1.ogg"),
-	"luigi_hit_2": preload("res://game_scenes/whack_mole/assets/audio/NCS_SE_VS_MOG_LUIGIHIT_2.ogg"),
-	"luigi_hit_3": preload("res://game_scenes/whack_mole/assets/audio/NCS_SE_VS_MOG_LUIGIHIT_3.ogg")
-}
-
-var hittable = preload("res://game_scenes/whack_mole/entities/hittable.tscn")
-
 
 func _ready():
-	DBs.SCENE_PRELOADS.sounds = SOUNDS
-	DBs.SCENE_PRELOADS.anim_luigi = preload("res://game_scenes/whack_mole/hittable_luigi.tres")
-	DBs.SCENE_PRELOADS.luigi_hits = 0
-	DBs.SCENE_PRELOADS.anim_speed = 1
+	pass
 
 
 func spawn_new_target():
-	var target: Area2D = hittable.instantiate()
-	target.is_luigi = randi() % 2
-	target.global_position = $"2D/BGBottom/Mask".get_children()[randi() % $"2D/BGBottom/Mask".get_child_count()].global_position
-	$"2D".add_child(target)
-	target.hit.connect($HUDOverlay._on_points_sum_specific.bind(target.points))
+	pass
 
 
 func _on_timer_timeout():
